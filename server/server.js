11be02b668e16a499765a7285a0d5958c4d61082
saads4 <saad.sayed.ssa@gmail.com>
2026-03-11 10:20:21 +0530

    Cors Issue origin changed

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://biosynvanta.vercel.app",
+      "https://biosynvanta.onrender.com",
       "http://localhost:5173"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
