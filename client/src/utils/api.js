4a1fe5ee2049a07e7d48bcf65a0c877a053152d7
pit-15 <pankitshah39@gmail.com>
2026-03-12 09:55:05 +0530

    Harcoded render URL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5051/api',  
+  baseURL:'https://biosynvanta.onrender.com/api',  
   timeout: 10000, // 10 second timeout
 });
 
